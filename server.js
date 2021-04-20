2e8ca363d935970ed5ef4ae7b7edc9b4f9bffcc3
Himanshu Ramoliya <50837805+himanshuramoliya@users.noreply.github.com>
2021-04-20 23:39:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,8 @@ const JWT_SECRET =
 //   useCreateIndex: true,
 // });
 
-const db =
-  "mongodb+srv://Vismay:nlsES0uop6G2zJ4H@cluster0.w3tty.mongodb.net/SE?retryWrites=true&w=majority";
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
